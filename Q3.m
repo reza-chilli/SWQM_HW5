7be3f9812d80aaf4a9acec7cbaa36c280e97bbaf
moonlight <moonlight@gmail.com>
2024-06-01 16:23:22 +0330

    some adjustments to fix analytical solution

--- Q3.m
+++ Q3.m
@@ -2,23 +2,23 @@ clc; clear;
 
 depth = 4; % m
 width_es = 100; % m
-velocity = 250; % m/hr
-E = 1000000; % m^2/hr
+velocity = 250/(60 * 60); % m/s
+E = 1000000/(60 * 60); % m^2/s
 W = 10; % kg
-k = 0.1/24; % 1/hr
+k = 0.1/(24 * 60 * 60); % 1/s
 area = depth * width_es; % m^2
-discharge = width_es * depth * velocity; % m^3/hr
+discharge = width_es * depth * velocity/(60 * 60); % m^3/s
 
-W_plane = W/area; % kg/m^2
+W_plane = (W/area) * 1000; % gr/m^2
 
-t = 3:7:24; % hr (inspect in a day with interval of 3 hr)
-x = 0:0.25:10; % m (inspect in 1 km every 20 m)
+t = 10800:25200:86400; % sec (inspect in a day with interval of 3 hr)
+x = 0:20:10000; % m (inspect in 10 km every 20 m)
 
-c_analytical = zeros(length(t), length(x));
+c_analytical = zeros(length(t), length(x)); % gr/m^3 or ppm
 
 for i = 1:size(c_analytical, 1)
     for j = 1:size(c_analytical, 2)
-        c_analytical(i, j) = (W_plane/(2 * ((pi * 0.1 * t(i))^ 0.5))) * exp(-(((x(j) - ((0.2 * t(i)))) ^ 2)/(4 * 0.1 * t(i))));
+        c_analytical(i, j) = (W_plane/(2 * ((pi * E * t(i))^ 0.5))) * exp(-(((x(j) - ((velocity * t(i)))) ^ 2)/(4 * E * t(i))) - (k * t(i)));
     end
 end
 
@@ -31,8 +31,8 @@ for i = 2:size(c_analytical, 1)
     plot(x, c_analytical(i, :));
 end
 xlabel('Length (m)');
-ylabel('Concentration (kg/m^3)');
-legend('t = 3', 't = 10', 't = 17', 't = 24');
+ylabel('Concentration (ppm)');
+legend('t = 3h', 't = 10h', 't = 17h', 't = 24h');
 hold off;
 
 
